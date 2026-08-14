1525131fd243e87ce1f435f9d379c56f772bef32
Koushik <koushiknandanuru@gmail.com>
2026-08-14 18:26:18 +0530

    Prepare Django application for deployment

--- BrainTumorWeb/BrainTumorWeb/settings.py
+++ BrainTumorWeb/BrainTumorWeb/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-&qrl38+u!wfo^fs&)8epaqm!e_8-t92t-n75k5xs8%$u4$)t)3'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # -------------------------
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +126,10 @@ USE_TZ = True
 # Static & Media Files
 # -------------------------
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Added
 STATICFILES_DIRS = [
